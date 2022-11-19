159dc525c8abf2bd585a06741f4dbb97df72b0cf
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-11-19 23:56:15 +0500

    Add a simple loading animation to the page

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ let elBtn = document.querySelector('.dark');
 elBtn.addEventListener('click', function () {
     elBody.classList.toggle('mode');
 })
+
+const loading = document.querySelector('.loader');
+const content = document.querySelector('body');
+setTimeout(() => {
+  loading.style.opacity = "0";
+  content.style.opacity = "1";
+}, 3000)
